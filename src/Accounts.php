f5ed54e6e79e5527b301d6be80a0cc170c9f570f
Maris Fisher <marisfisher@hotmail.co.uk>
2020-01-29 16:23:46 +0000

    :sparkles Updated account & event crud operations

--- src/Accounts.php
+++ src/Accounts.php
@@ -9,4 +9,25 @@ class Accounts extends CuroInterface
         $out = $this->getClientEndpoint('/api/v1/accounts', $parameters);
         return (object) $out;
     }
+
+    public function store(array $parameters = [])
+    {
+        $this->postUserEndpoint('/api/v1/accounts', 'POST', $parameters);
+        $this->clearCache('/api/v1/accounts');
+    }
+
+    public function update(string $account_uuid, array $parameters = [])
+    {
+        $out = $this->postClientEndpoint('/api/v1/accounts/' . $account_uuid, 'PUT', $parameters);
+        $this->clearCache('/api/v1/accounts');
+        $this->clearCache('/api/v1/accounts/' . $account_uuid);
+        return (object) $out;
+    }
+
+    public function delete(int $account_id)
+    {
+        $out = $this->postUserEndpoint('/api/v1/accounts/' . $account_id, 'DELETE');
+        $this->clearCache('/api/v1/accounts');
+        $this->clearCache('/api/v1/accounts/' . $account_id);
+    }
 }
--- src/Events.php
+++ src/Events.php
@@ -6,13 +6,34 @@ class Events extends CuroInterface
 {
     public function getEvents($parameters)
     {
-        $out = $this->getClientEndpoint('/api/events', $parameters);
+        $out = $this->getClientEndpoint('/api/v1/events', $parameters);
 
         return (object) $out;
     }
 
     public function book($parameters)
     {
-        $this->postClientEndpoint('/api/events/' . $parameters['event_id'] . '/book', 'POST', $parameters);
+        $this->postClientEndpoint('/api/v1/events/' . $parameters['event_id'] . '/book', 'POST', $parameters);
+    }
+
+    public function store(array $parameters = [])
+    {
+        $this->postUserEndpoint('/api/v1/events', 'POST', $parameters);
+        $this->clearCache('/api/v1/events');
+    }
+
+    public function update(string $event_uuid, array $parameters = [])
+    {
+        $out = $this->postClientEndpoint('/api/v1/events/' . $event_uuid, 'PUT', $parameters);
+        $this->clearCache('/api/v1/events');
+        $this->clearCache('/api/v1/events/' . $event_uuid);
+        return (object) $out;
+    }
+
+    public function delete(int $event_id)
+    {
+        $out = $this->postUserEndpoint('/api/v1/events/' . $event_id, 'DELETE');
+        $this->clearCache('/api/v1/events');
+        $this->clearCache('/api/v1/events/' . $event_id);
     }
 }
